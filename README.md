9c836b00e12c304b61c79ccd3471fdcd093125c7
joshualena <lenajo@oregonstate.edu>
2023-11-19 17:31:58 -0700

    Added Google Places API backend

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# CS-361-Portfolio
+# StockInsightCLI
+
+Welcome to StockInsightCLI, a Python-based command-line interface that provides you with real-time insights into the stock market. This intuitive tool allows you to track top-performing and bottom-performing stocks, as well as fetch the daily opening and closing prices of your stocks of interest.
+
+## Features
+
+- **Top Stocks**: Fetch data about the stock market's top performers.
+- **Bottom Stocks**: Retrieve information on the stock market's bottom performers.
+- **Daily Open/Close**: Get the latest opening and closing prices for a specified stock.
+- **Alerts**: (Upcoming Feature) Set up real-time alerts for stock performance thresholds.
+- **News**: (Upcoming Feature) Stay updated with the latest financial news.
+
+## USAGE
+
+After starting the application, you will be presented with a command prompt. Use the following commands to interact with the application:
+
+- **top-stocks**: Displays the top 5 performing stocks.
+
+- **bottom-stocks**: Shows the bottom 5 performing stocks.
+
+- **daily-open-close**: Fetches the most recent open and close prices for a specified stock.
+
+- **alerts**: (To be implemented) Set up custom trading alerts.
+
+- **news**: (To be implemented) Access the latest financial news.
+
+- **help**: Provides a list of all available commands.
+
+- **exit**: Closes the application.
--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify, request
 import requests
+import os
 
 app = Flask(__name__)
 
-# API key should be kept secure. Consider loading from environment for production.
-api_key = 'S4U9D9FVFLIA63BF'
-
+# Load the API key for Alpha Vantage and Google Places API from environment variables for security
+alpha_vantage_api_key = os.getenv('ALPHA_VANTAGE_API_KEY')
+google_places_api_key = os.getenv('GOOGLE_PLACES_API_KEY')
 
 @app.route('/top_stocks', methods=['GET'])
 def get_top_stocks():
@@ -18,7 +19,6 @@ def get_top_stocks():
     print(f"Received request at {request.path}")
     return get_stock_performance('top')
 
-
 @app.route('/bottom_stocks', methods=['GET'])
 def get_bottom_stocks():
     """Endpoint to retrieve bottom performing stocks.
@@ -27,7 +27,6 @@ def get_bottom_stocks():
         A JSON response containing the bottom performing stock data or an error message.
     """
     return get_stock_performance('bottom')
-
 
 @app.route('/daily_open_close', methods=['GET'])
 def get_daily_open_close():
@@ -40,7 +39,7 @@ def get_daily_open_close():
     """
     symbol = request.args.get('symbol', 'AAPL')
     function = "TIME_SERIES_DAILY"
-    alpha_vantage_url = f'https://www.alphavantage.co/query?function={function}&symbol={symbol}&apikey={api_key}'
+    alpha_vantage_url = f'https://www.alphavantage.co/query?function={function}&symbol={symbol}&apikey={alpha_vantage_api_key}'
 
     try:
         response = requests.get(alpha_vantage_url)
@@ -60,16 +59,12 @@ def get_daily_open_close():
             'close': close_price
         })
     except requests.HTTPError as http_err:
-        # HTTP errors are caught and sent back with a 500 status code.
         return jsonify({'error': f'HTTP error occurred: {http_err}'}), 500
     except Exception as err:
-        # Any other exceptions are caught and a generic error message is returned.
         return jsonify({'error': f'An unexpected error occurred: {err}'}), 500
 
 # TODO: News Route
-
 # TODO: Alerts Route
-
 
 def get_stock_performance(stock_category):
     """
@@ -82,36 +77,23 @@ def get_stock_performance(stock_category):
     Returns:
     A JSON response with the stock performance data or an error message.
     """
-    if stock_category == 'top':
-        alpha_vantage_function = 'TOP_GAINERS_LOSERS'
-        key_in_response = 'top_gainers'
-    elif stock_category == 'bottom':
-        alpha_vantage_function = 'TOP_GAINERS_LOSERS'
-        key_in_response = 'top_losers'
-    else:
-        # If an invalid stock_category is specified, return an error.
-        return jsonify({'error': 'Invalid stock_category specified'}), 400
+    alpha_vantage_function = 'TOP_GAINERS_LOSERS'
+    key_in_response = 'top_gainers' if stock_category == 'top' else 'top_losers'
 
-    alpha_vantage_url = f'https://www.alphavantage.co/query?function={alpha_vantage_function}&apikey={api_key}'
+    alpha_vantage_url = f'https://www.alphavantage.co/query?function={alpha_vantage_function}&apikey={alpha_vantage_api_key}'
 
     try:
-        # Performs the API request and handles potential HTTP errors.
         response = requests.get(alpha_vantage_url)
         response.raise_for_status()
         data = response.json()
 
-        # Checks if the API returned an error message.
         if 'Error Message' in data:
             return jsonify({'error': data['Error Message']}), 400
 
-        # Extracts the relevant data from the API response.
         relevant_data = data.get(key_in_response)
-
-        # If the expected data is not present, return an error.
         if relevant_data is None:
             return jsonify({'error': 'No data available'}), 404
 
-        # Returns the relevant data as a JSON response.
         return jsonify(relevant_data)
     except requests.HTTPError as http_err:
         # Returns a JSON error message for HTTP errors.
@@ -119,7 +101,27 @@ def get_stock_performance(stock_category):
     except Exception as err:
         # Catches any other exceptions and returns a generic error message.
         return jsonify({'error': f'An unexpected error occurred: {err}'}), 500
+    
+@app.route('/search_places', methods=['GET'])
+def search_places():
+    """Endpoint to search for places using Google Places API."""
+    query = request.args.get('query', 'pet shelters in Oregon')
 
+    if not query:
+        return jsonify({'error': 'Query parameter is required'}), 400
+
+    google_places_url = f'https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&key={google_places_api_key}'
+
+    try:
+        response = requests.get(google_places_url)
+        response.raise_for_status()
+        data = response.json()
+        return jsonify(data)
+    except requests.HTTPError as http_err:
+        return jsonify({'error': f'HTTP error occurred: {http_err}'}), 500
+    except Exception as err:
+        return jsonify({'error': f'An unexpected error occurred: {err}'}), 500
+    
 
 # Runs the application in debug mode when the script is executed directly.
 if __name__ == '__main__':
